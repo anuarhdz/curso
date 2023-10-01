f2a521920423cb3142918efbc2db09a5e5044fe2
Anuar Reyes <anuar@hey.com>
2023-10-01 10:34:16 -0600

    initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+let userName = 'Anuar';
+let userAge = 28;
+let names = [ 'Anuar', 'Juan', 'Luis', 'Julieta', 34, 80, [1, 2] ];
+console.log(names);
+names[0] = 'Ruben';
+console.log(names);
+console.log(names[0]);
